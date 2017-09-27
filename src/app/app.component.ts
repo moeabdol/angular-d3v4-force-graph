20b14d81b9b5f6f0dc9bb7eb523ae26bc3c8984a
moeabdol <mohd.a.saed@gmail.com>
2017-09-27 17:04:39 +0300

    Install d3 and @types/d3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    console.log('D3.js version:', d3['version']);
+  }
 }
